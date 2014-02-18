brigade/rails

commit 4f2cd3e9fee721d7485bb1c1b4151612d733da3a

group things that are alike

actionpack/lib/action_controller/metal/testing.rb
@@ -17,6 +17,9 @@ module ActionController
 
       def recycle!
         @_url_options = nil
+        self.response_body = nil
+        self.formats = nil
+        self.params = nil
       end
     end
 

actionpack/lib/action_controller/test_case.rb
@@ -467,11 +467,14 @@ module ActionController
         # proper params, as is the case when engaging rack.
         parameters = paramify_values(parameters) if html_format?(parameters)
 
+        unless @controller.respond_to?(:recycle!)
+          @controller.extend(Testing::Functional)
+          @controller.class.class_eval { include Testing }
+        end
+
         @request.recycle!
         @response.recycle!
-        @controller.response_body = nil
-        @controller.formats = nil
-        @controller.params = nil
+        @controller.recycle!
 
         @html_document = nil
         @request.env['REQUEST_METHOD'] = http_method
@@ -492,12 +495,6 @@ module ActionController
 
         build_request_uri(action, parameters)
 
-        unless @controller.respond_to?(:recycle!)
-          @controller.extend(Testing::Functional)
-          @controller.class.class_eval { include Testing }
-        end
-
-        @controller.recycle!
         name = @request.parameters[:action]
 
         @controller.process(name)
@@ -535,7 +532,7 @@ module ActionController
         setup :setup_controller_request_and_response
       end
 
-    private
+      private
       def check_required_ivars
         # Sanity check for required instance variables so we can give an
         # understandable error message.

commit 50e0eb2b3dc4c94a707c8c6a6eec6ef4ae5388f1

Fix controller test not resetting @_url_options
Commit 4f2cd3e9 introduced a bug by reordering the call to
`@controller.recycle!` above the call to `build_request_uri`. The
impact of this was that the `@_url_options` cache ends up not being
reset between building a request URI (occurring within the test
controller) and the firing of the actual request.

We encountered this bug because we had the following setup:

  class MinimumReproducibleController < ActionController::Base
    before_filter { @param = 'param' }

    def index
      render text: url_for(params)
    end

    def default_url_options
      { custom_opt: @param }
    end
  end

  def test_index
    get :index # builds url, then fires actual request
  end

The initial call to `get :index` in the test suite would populate the
@_url_options cache. The subsequent call to `url_for` would depend on
the cache, thus never calling the now-updated default_url_options.

This commit fixes this bug calling recycle! twice, and removes a call
to set response_body, which should no longer be needed since we're
recycling the request object explicitly.

actionpack/lib/action_controller/metal/testing.rb
@@ -17,7 +17,6 @@ module ActionController
 
       def recycle!
         @_url_options = nil
-        self.response_body = nil
         self.formats = nil
         self.params = nil
       end

actionpack/lib/action_controller/test_case.rb
@@ -566,6 +566,7 @@ module ActionController
 
         name = @request.parameters[:action]
 
+        @controller.recycle!
         @controller.process(name)
 
         if cookies = @request.env['action_dispatch.cookies']

actionpack/test/controller/test_case_test.rb
@@ -162,6 +162,31 @@ XML
     end
   end
 
+  class DefaultUrlOptionsCachingController < ActionController::Base
+    before_filter do
+      @dynamic_opt = 'opt'
+    end
+
+    def test_url_options_not_cached
+      render text: url_for(params)
+    end
+
+    def default_url_options
+      if defined?(@dynamic_opt)
+        { :dynamic_opt => @dynamic_opt }
+      else
+        {}
+      end
+    end
+  end
+
+  def test_url_options_reset
+    @controller = DefaultUrlOptionsCachingController.new
+    get :test_url_options_not_cached
+    assert_nil @request.params['dynamic_opt']
+    assert_match(/dynamic_opt=opt/, @response.body)
+  end
+
   def test_raw_post_handling
     params = Hash[:page, {:name => 'page name'}, 'some key', 123]
     post :render_raw_post, params.dup